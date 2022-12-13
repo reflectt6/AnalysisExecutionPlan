4b4521ba9573ee82a468ee7de8543aeac4ac1f15
reflectt6 <reflectt6@gmail.com>
2022-12-13 17:11:52 +0800

    2

--- analysis_utils.py
+++ analysis_utils.py
@@ -5,7 +5,7 @@ import time
 SECONDS_PER_MINUTE = 60
 
 
-def get_spark_logs(start_time='1940-01-01 00：00', end_time='2500-01-01 00：00', spark_history_path='/spark2-history'):
+def get_spark_logs(start_time='1940-01-01 00:00', end_time='2500-01-01 00:00', spark_history_path='/spark2-history'):
     """
     获取spark日志
     :param start_time: 日志开始时间段
@@ -14,8 +14,8 @@ def get_spark_logs(start_time='1940-01-01 00：00', end_time='2500-01-01 00：00
     :return:
     """
     (ret, logs_name, err) = run_cmd(['hdfs', 'dfs', '-ls', spark_history_path])
-    start_time = time.strptime(start_time, "%Y-%m-%d %H:%M") - SECONDS_PER_MINUTE
-    end_time = time.strptime(end_time, "%Y-%m-%d %H:%M") + SECONDS_PER_MINUTE
+    start_time = int(time.strptime(start_time, "%Y-%m-%d %H:%M")) - SECONDS_PER_MINUTE
+    end_time = int(time.strptime(end_time, "%Y-%m-%d %H:%M")) + SECONDS_PER_MINUTE
     yarn_logs = {}
     assert ret == 0
     for item in logs_name[1:]:
@@ -23,7 +23,7 @@ def get_spark_logs(start_time='1940-01-01 00：00', end_time='2500-01-01 00：00
         log_name = log_path.split('/')[-1]
         match = re.search(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}', item)
         if match and 'inprogress' not in log_path and 'local' not in log_path:
-            cur_time = time.strptime(match.group(), "%Y-%m-%d %H:%M")
+            cur_time = int(time.strptime(match.group(), "%Y-%m-%d %H:%M"))
             if start_time < cur_time < end_time:
                 yarn_logs[log_name] = cur_time
     return yarn_logs
